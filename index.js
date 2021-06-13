e00b503e3eb9bfb6459b1d5e96e7e6684718267d
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-13 12:57:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+let element = document.createElement('div')
+
+document.body.appendChild(element);
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+element.appendChild(newHeader)
